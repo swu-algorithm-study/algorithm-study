3a04d475c0d2e065a4c7ec60b4d53ca811ff541d
starday119 <jy8294@naver.com>
2025-05-02 21:25:52 +0900

    ✨ FEAT: DFS/BFS 백준 실버 어렵다...

--- 3184.py
+++ 3184.py
@@ -1,46 +1,53 @@
 from collections import deque
- 
+
+# 방향 벡터: 상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# 입력 처리
 R, C = map(int, input().split())
-B = [input() for _ in range(R)]
- 
-visit = [[False] * C for _ in range(R)]
-queue = deque()
- 
-dr = [1, -1, 0, 0]
-dc = [0, 0, 1, -1]
- 
-final_o, final_v = 0, 0
-for i in range(R) :
-    for j in range(C) :
-        if B[i][j] != '#' and not visit[i][j] :
-            queue.append((i, j))
-            visit[i][j] = True
- 
-            o_count = 1 if B[i][j] == 'o' else 0
-            v_count = 1 if B[i][j] == 'v' else 0
- 
-            while len(queue) != 0 :
-                r, c = queue.popleft()
- 
-                for k in range(4) :
-                    nr, nc = r + dr[k], c + dc[k]
- 
-                    if nr < 0 or R <= nr or nc < 0 or C <= nc :
-                        continue
-                    if B[nr][nc] == '#' :
-                        continue
-                    if not visit[nr][nc] :
-                        queue.append((nr, nc))
-                        visit[nr][nc] = True
- 
-                        if B[nr][nc] == 'o' :
-                            o_count += 1
-                        if B[nr][nc] == 'v' :
-                            v_count += 1
-            
-            if o_count > v_count :
-                final_o += o_count
-            else :
-                final_v += v_count
- 
-print(final_o, final_v)
+field = [list(input().strip()) for _ in range(R)]
+visited = [[False] * C for _ in range(R)]
+
+total_sheep = 0
+total_wolf = 0
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = True
+
+    sheep = 0
+    wolf = 0
+
+    if field[x][y] == 'o':
+        sheep += 1
+    elif field[x][y] == 'v':
+        wolf += 1
+
+    while queue:
+        cx, cy = queue.popleft()
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if 0 <= nx < R and 0 <= ny < C:
+                if not visited[nx][ny] and field[nx][ny] != '#':
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+                    if field[nx][ny] == 'o':
+                        sheep += 1
+                    elif field[nx][ny] == 'v':
+                        wolf += 1
+    return sheep, wolf
+
+# 전체 마당 탐색
+for i in range(R):
+    for j in range(C):
+        if not visited[i][j] and field[i][j] != '#':
+            sheep, wolf = bfs(i, j)
+            if sheep > wolf:
+                total_sheep += sheep
+            else:
+                total_wolf += wolf
+
+print(f"{total_sheep} {total_wolf}")
